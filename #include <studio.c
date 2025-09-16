0e3b9b6396613819df6b2b5d54c72d537e41ab59
thalescardoso0211 <thales_cardoso0211@hotmail.com>
2025-09-16 00:24:40 +0000

    criei os loops !

---
+++ #include <studio.c
@@ -0,0 +1,18 @@
+
+int main(){
+    int b = 1;
+
+    do{
+        printf("mover direita diagonal\n", b);
+        b++;
+    }while(b <= 3);
+
+
+
+    do{
+        printf("mover esquerda diagonal\n", b);
+        b++;
+    }while(b <= 1);
+
+    return 0;
+}
